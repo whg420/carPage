dce08010123dd8c2862a4a02a708ae44f8340ab6
whg420 <55119058+whg420@users.noreply.github.com>
2019-12-03 11:05:06 +0800

    666

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-    
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-   width: 100%;
-   height: 100%;
+*{
+  margin: 0;
+  padding: 0;
+  appearance: none;
+  list-style: none;
+  box-sizing: border-box
 }
+html,body{
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 
+}
 
 </style>
---
+++ src/views/Detaile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+detaile
+ {{this.$route.query.ID}}
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
